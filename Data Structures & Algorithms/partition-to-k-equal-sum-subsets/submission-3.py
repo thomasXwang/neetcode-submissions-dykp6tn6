86b9b6481a1517ef7f4b17a54ffff717a4c22080
Thomas <20924993+thomasXwang@users.noreply.github.com>
2026-06-25 01:09:27 +0200

    Add: partition-to-k-equal-sum-subsets - submission-3

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        
+        n = len(nums)
+
+        total = sum(nums)
+        if total % k != 0:
+            return False
+        target = total // k
+
+        nums.sort(reverse=True)
+        if nums[0] > target:
+            return False
+
+        subsets = [0] * k
+
+        def dfs(i):
+            if i == n:
+                return True
+
+            seen_subset_sums = set()
+
+            for j in range(k):
+                if subsets[j] in seen_subset_sums:
+                    continue
+                
+                seen_subset_sums.add(subsets[j])
+
+                if subsets[j] + nums[i] <= target:
+                    subsets[j] += nums[i]
+                    if dfs(i + 1):
+                        return True
+                    subsets[j] -= nums[i]
+
+                    # if subsets[j] == 0:
+                    #     break
+
+            return False
+
+        return dfs(0)
